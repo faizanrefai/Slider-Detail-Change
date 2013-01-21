0685e2685526337b28639b6a185d4fe6c8dc43c4
Faizan Refai <faizanrefai@gmail.com>
2013-01-22 01:04:17 +0530

    Initial Commit

---
+++ CommonClassSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CommonClassSample
+//
+//  Created by MacBook on 1/22/13.
+//  Copyright (c) 2013 iPatientCare MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
